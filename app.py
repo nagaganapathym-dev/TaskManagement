29cea0ee0169db53de9c1132c7649146e5b19920
Nagaganapathy M <nagaganapathym@gmail.com>
2026-07-13 09:06:11 +0530

    Fix database initialization for Render

--- app.py
+++ app.py
@@ -258,8 +258,8 @@ def logout():
     return redirect(url_for("home"))
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
