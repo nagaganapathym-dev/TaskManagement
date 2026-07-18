9958229029af0a79e75655e990a6b08b68b1736d
Nagaganapathy M <nagaganapathym@gmail.com>
2026-07-18 21:47:43 +0530

    Configure PostgreSQL database

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "mysecretkey")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskmanager.db"
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///taskmanager.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
